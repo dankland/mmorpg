bceae4f369111bdf7a9b39a8f793c428df72c07d
Manik Charan <mkchan2951@gmail.com>
2023-05-10 02:03:20 +0530

    Fix namespace and make an example ecs application on server

--- core/component/position.hpp
+++ core/component/position.hpp
@@ -1,13 +1,13 @@
 #ifndef MMORPG_CORE_COMPONENT_POSITION_HPP
 #define MMORPG_CORE_COMPONENT_POSITION_HPP
 
-namespace mmorpg {
+namespace core::component {
 
 struct Position {
     float x = 0.0f;
     float y = 0.0f;
 };
 
-}  // namespace mmorpg
+}  // namespace core::component
 
 #endif  // MMORPG_CORE_COMPONENT_POSITION_HPP
--- server/server.cpp
+++ server/server.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
+#include "flecs.h"
+
 #include "core/component/position.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void setup_entities(flecs::world& ecs) {
+    ecs.entity("ExampleEntity").set<core::component::Position>({10, 20});
+}
 
-    mmorpg::Position position;
-    std::cout << "Position: " << position.x << ", " << position.y << std::endl;
+void setup_systems(flecs::world& ecs) {
+    ecs.system<const core::component::Position>()
+        .kind(flecs::OnUpdate)
+        .each([](const core::component::Position& position) {
+            std::cout << "Position: " << position.x << ", " << position.y << std::endl;
+        });
+}
+
+int main() {
+    flecs::world ecs;
+
+    setup_entities(ecs);
+    setup_systems(ecs);
+
+    ecs.app().target_fps(60).run();
 
     return 0;
 }
